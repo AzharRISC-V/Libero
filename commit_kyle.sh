f7527298d3c11525245169c34bf5fd19743de77c
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2020-07-13 14:21:20 -0700

    auto-commit : Update by Kyle at  14:21 on 07/13/2020

---
+++ commit_kyle.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cd D:/Project-499/Libero
+git add --all
+timestamp() {
+  date +"at  %H:%M on %m/%d/%Y"
+}
+git commit -am "auto-commit : Update by Kyle $(timestamp)"
+git pull
+git push origin master
